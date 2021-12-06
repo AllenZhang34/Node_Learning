82dc3a51ef537843d0b6627e69208a7acff55177
Allen Zhang <allen.zhang07@sap.com>
2021-12-06 10:33:58 +0800

    Global object

---
+++ Global objects/__dirname.js
@@ -0,0 +1,2 @@
+console.log(__dirname);
+// /Users/i337605/Documents/GitHub/Node_Learning/Global objects
---
+++ Global objects/__filename.js
@@ -0,0 +1,2 @@
+console.log(__filename);
+// /Users/i337605/Documents/GitHub/Node_Learning/Global objects/__filename.js
